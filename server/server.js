77caddd50701be92423d315b9266108f8d67ac2d
sharadbhat <sharad.mbhat@gmail.com>
2021-01-25 15:56:08 +0530

    info socket

--- server/server.js
+++ server/server.js
@@ -18,6 +18,10 @@ io.on('connection', (socket) => {
 
     console.log('made socket connection', socket.id)
 
+    socket.on('info', function(data) {
+        io.sockets.emit('info', data)
+    })
+
     // Handle chat event
     socket.on('message', function(data) {
         io.sockets.emit('message', data)
